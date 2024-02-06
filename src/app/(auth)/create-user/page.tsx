d3f764ae294247b75e4b35e2601a92c31d723feb
Jugya K. Gogoi <crjugya123@gmail.com>
2024-02-06 21:43:36 +0530

    test

--- src/app/(auth)/create-user/page.tsx
+++ src/app/(auth)/create-user/page.tsx
@@ -24,7 +24,7 @@ const formSchema = z.object({
   username: z
     .string()
     .min(2, {
-      message: "Username must be at least 2 characters long",
+      message: "Username must be at least 1 characters long",
     })
     .max(20, {
       message: "Username must be at most 20 characters long",
