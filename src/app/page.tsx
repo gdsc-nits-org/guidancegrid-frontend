5d4ccdb4ddf04efc94cce1afc3cb9ac27f52793f
Jaydeep Das <jaydeepjd.8914@gmail.com>
2024-02-09 12:28:31 +0530

    Modified Home Page
    Minor change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const { push } = useRouter();
   return (
-    <div className="prose h-full flex flex-col md:flex-row justify-center items-center ">
+    <div className="prose h-full flex flex-col md:flex-row justify-center items-center gap-x-2">
       <h1>GUIDANCE GRID FRONTEND</h1>
-    <Link href={"/verify"} className="p-2 border border-black rounded m-3 hover:bg-black hover:text-white">Sign up</Link>
-    <Link href={"/login"} className="p-2 border border-black rounded m-3 hover:bg-black hover:text-white">Log in</Link>
+      <Link href={"/verify"}>
+        <Button>Sign Up</Button>
+      </Link>
+      <Link href={"/login"}>
+        <Button>Login</Button>
+      </Link>
     </div>
   );
 }
